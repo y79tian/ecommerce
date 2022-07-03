c5a1ba53d665c8f043cad91e3c407d087d7dacb4
Yang Tian <yang.tian@fintelics.com>
2022-07-03 00:24:48 -0400

    set db and models

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 import express from "express"; // common js by default, not ES modules (as we used in the frontend)
 import dotenv from "dotenv";
-import products from "./data/products.js"; // must add .js if we would like to import a file  
+import colors from "colors";
+import products from "./data/products.js"; // must add .js if we would like to import a file
+import connectDB from "./config/db.js";
 
 dotenv.config();
+connectDB();
 
 const app = express();
 
@@ -22,5 +25,5 @@ app.get("/api/products/:id", (req, res) => {
 const PORT = process.env.PORT || 4000;
 app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
 );
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Row, Col } from "react-bootstrap";
-// import products from "../products";
 import Product from "../components/Product";
 
 const HomeScreen = () => {
