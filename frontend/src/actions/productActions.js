a1acfe406e84ad125846898b2bdefec222c61129
Yang Tian <yang.tian@fintelics.com>
2022-07-04 13:24:05 -0400

    add redux for product detail

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -3,6 +3,9 @@ import {
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
   PRODUCT_LIST_FAIL,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
+  PRODUCT_DETAILS_FAIL,
 } from "../constants/productConstants.js";
 
 // consider the function below as action creator, the constants above stands for the actions
@@ -17,6 +20,24 @@ export const listProducts = () => async (dispatch) => {
     // handle error
     dispatch({
       type: PRODUCT_LIST_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
+
+export const listProductDetails = (id) => async (dispatch) => {
+  try {
+    // when you call dispatch,it triggers the function in the reducer
+    dispatch({ type: PRODUCT_DETAILS_REQUEST });
+    const { data } = await axios.get(`/api/products/${id}`);
+    dispatch({ type: PRODUCT_DETAILS_SUCCESS, payload: data });
+  } catch (error) {
+    // handle error
+    dispatch({
+      type: PRODUCT_DETAILS_FAIL,
       payload:
         error.response && error.response.data.message
           ? error.response.data.message
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,15 +1,16 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension"; // used for the extension in the chrome
-import {productListReducer} from './reducers/productReducers.js';
+import { productListReducer,productDetailsReducer } from "./reducers/productReducers.js";
 
 const reducer = combineReducers({
-    productList: productListReducer // product list is what will be shown as a piece of state
+  productList: productListReducer, // product list is what will be shown as a piece of state
+  productDetails: productDetailsReducer,
 });
 
 const initialState = {};
 
-const middleware = [thunk];
+const middleware = [thunk]; // thunk allows you to use async functions
 
 const store = createStore(
   reducer,
