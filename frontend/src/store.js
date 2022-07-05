51ede52609d52ae9a5dfc7d77d0538e929a1ea4e
Yang Tian <yang.tian@fintelics.com>
2022-07-05 12:37:48 -0400

    add user reducer and action

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -4,23 +4,33 @@ import { composeWithDevTools } from "redux-devtools-extension"; // used for the 
 import {
   productListReducer,
   productDetailsReducer,
-} from "./reducers/productReducers.js";
-import { cartReducer } from "./reducers/cartReducers.js";
+} from "./reducers/productReducers";
+import { cartReducer } from "./reducers/cartReducers";
+import { userLoginReducer } from "./reducers/userReducers";
 
+// reducer
 const reducer = combineReducers({
   productList: productListReducer, // product list is what will be shown as a piece of state
   productDetails: productDetailsReducer,
   cart: cartReducer,
+  userLogin: userLoginReducer,
 });
 
+// initial state
 const cartItemsFromStorage = localStorage.getItem("cartItems")
   ? JSON.parse(localStorage.getItem("cartItems"))
   : [];
 
+const userInfoFromStorage = localStorage.getItem("userInfo")
+  ? JSON.parse(localStorage.getItem("userInfo"))
+  : null;
+
 const initialState = {
-  cart: {cartItems: cartItemsFromStorage}
+  cart: { cartItems: cartItemsFromStorage },
+  userLogin: { userInfo: userInfoFromStorage },
 };
 
+// middleware
 const middleware = [thunk]; // thunk allows you to use async functions
 
 const store = createStore(
