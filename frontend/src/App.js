9366c3459ba0ce50d6e1ebebf809430e2641c606
Yang Tian <yang.tian@fintelics.com>
2022-07-05 15:57:59 -0400

    finish user logout and login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,8 @@ import Footer from "./components/Footer";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
+import LoginScreen from "./screens/LoginScreen";
+
 function App() {
   return (
     <Router>
@@ -13,11 +15,12 @@ function App() {
       <main className="py-3">
         <Container>
           <Routes>
-            <Route path="/" element={<HomeScreen />} />
+          <Route path="/login" element={<LoginScreen />} />
             <Route path="/product/:id" element={<ProductScreen />} />
             <Route path="cart" element={<CartScreen />}>
               <Route path=":id" element={<CartScreen />} />
             </Route>
+            <Route path="/" element={<HomeScreen />} />
           </Routes>
         </Container>
       </main>
