b3b6a97d04acc162e3dc9f12e5789e8d5a824554
Yang Tian <yang.tian@fintelics.com>
2022-07-09 22:21:09 -0400

    update

--- backend/server.js
+++ backend/server.js
@@ -20,10 +20,6 @@ if (process.env.NODE_ENV === "development") {
 }
 app.use(express.json()); // req.body in the form of json
 
-app.get("/", (req, res) => {
-  res.send("API is running..");
-});
-
 app.use("/api/products", productRouter);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
@@ -36,6 +32,17 @@ app.get("/api/config/paypal", (req, res) => {
 // __dirname -> curr direct name, only used in commonjs, use path.resolve()
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads"))); // make the uploads folder static
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
 
 // if original url does not match any of the url above, it goes to the below error handler
 app.use(notFound);
